79390d6fd43e7ef6607bf087fea4b5694492777e
Shrayash B <99594645+shrayash25@users.noreply.github.com>
2025-01-17 19:21:11 +0530

    Update React app build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shrayash25.github.io/',  // Set this to your repository name with a slash
+  base: '/shrayash25.github.io/', // Your GitHub Pages repo name
 });
