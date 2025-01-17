0f8e61542ce6a53887fd08cba0285e6388257ac6
Shrayash B <99594645+shrayash25@users.noreply.github.com>
2025-01-17 11:53:31 +0530

    Configured base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/shrayash25.github.io/',  // Set this to your repository name with a slash
+});
